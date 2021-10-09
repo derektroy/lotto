e296e81c444d610a85ffb9404cc63903caed24b3
derektroy <L00170167@student.lyit.ie>
2021-10-09 09:26:32 +0100

    The Start

---
+++ Lotto_Python.py
@@ -0,0 +1,11 @@
+"""
+#
+# File          :   Lotto_Python.py
+# Created       :    
+# Author        :   Derek Troy
+# Version       :   1.0.0
+# Licensing     :   (C) 2021 Derek Troy LYIT
+#                   Available under GNU Public License (GPL)
+# Description   :
+#
+"""
