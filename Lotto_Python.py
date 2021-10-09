0d8c8e6b6543089fe946b5bae5b5da1c47dd20b1
derektroy <L00170167@student.lyit.ie>
2021-10-09 10:16:15 +0100

    First Lists Commit

--- Lotto_Python.py
+++ Lotto_Python.py
@@ -9,3 +9,12 @@
 # Description   :
 #
 """
+import random
+
+myList = [random.randint(1, 20) for _ in range(6)] # A random number generator for a list of 6 numbers between 1 and 20, outputted to myList
+
+ten_list = ["Tom", "Ivan", "Philly", "Seamus", "Mags", "Eileen", "Dave", "Biddy", "John", "James"] #A named list with 10 Lottery Players
+
+print(myList)
+print(ten_list)
+print("P")
